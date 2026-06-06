5518571a664419fe84595090d231db5a19e825e0
Dev∅ne <alles.empresarial@gmail.com>
2026-06-06 15:48:02 -0300

    maze in java games
    simple maze game

---
+++ Demonstration/echoes-labirinto/modelLabirinto.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.util.*;
+import java.awt.*;
+
+public class modelLabirinto extends JOptionPane{
+
+    public static void main(String[] args) {
+        private final int passX = 0;
+        private final int passY = 0;
+    }
+    
+}
